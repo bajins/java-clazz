c018822b40e7d496c59d88241e0884022572b406
woytu <394585829@qq.com>
2020-07-28 19:41:47 +0800

    add 设置默认的未捕获异常处理程序使用示例

---
+++ src/com/bajins/clazz/GoThread.java
@@ -0,0 +1,26 @@
+package com.bajins.clazz;
+
+public class GoThread {
+    public static void main(String[] args) {
+        // https://blog.csdn.net/wild46cat/article/details/89076832
+        /* 在以下两种方法中如果抛出了新的异常则会被JVM忽略。*/
+        // 捕获所有程序为直接处理的，由虚拟机抛出的异常。
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("出现异常");
+                e.printStackTrace();
+            }
+        });
+
+        int a = 10 / 0;
+        System.out.println(a);
+
+        Thread t = new Thread();
+        // 捕获该线程中抛出的程序本身未处理的异常。
+        t.setUncaughtExceptionHandler((t1, e) -> {
+            System.out.println("出现异常");
+            e.printStackTrace();
+        });
+    }
+}
