a18cd6908200c04c80f823641bda1dadbe9851cb
woytu <394585829@qq.com>
2020-09-22 17:08:42 +0800

    Update EncryptUtil.java

--- src/com/bajins/clazz/EncryptUtil.java
+++ src/com/bajins/clazz/EncryptUtil.java
@@ -253,17 +253,6 @@ public class EncryptUtil {
         System.out.println(encodedText);
         //解码
         System.out.println(new String(decoder.decode(encodedText), "UTF-8"));
-
-        final Base64.Decoder decoder = Base64.getDecoder();
-        final Base64.Encoder encoder = Base64.getEncoder();
-        final String text = "字串文字";
-        final byte[] textByte = text.getBytes("UTF-8");
-        //编码
-        final String encodedText = encoder.encodeToString(textByte);
-        System.out.println(encodedText);
-        //解码
-        System.out.println(new String(decoder.decode(encodedText), "UTF-8"));
-
     }
 
 }
