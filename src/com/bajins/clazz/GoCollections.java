5dac091b308d8b1054c128e03366d5a0ec183b88
woetu <admin@woetu.com>
2020-11-10 14:15:11 +0800

    add Java8 stream将一个list按三个一组分成N个小的list

--- src/com/bajins/clazz/GoCollections.java
+++ src/com/bajins/clazz/GoCollections.java
@@ -3,6 +3,8 @@ package com.bajins.clazz;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GoCollections {
     public static void main(String[] args) {
@@ -16,5 +18,26 @@ public class GoCollections {
         Collections.reverse(list); // 倒序排列
         Collections.shuffle(list); // 混乱排序
         List<Object> objects = Collections.emptyList();
+
+        List<Integer> intList =new ArrayList<>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(4);
+        intList.add(5);
+        // 将一个list按三个一组分成N个小的list，分组后的list不再是原list的视图，原list的改变不会影响分组后的结果
+
+        // 通过grouping by
+        Map<Integer, List<Integer>> groups = intList.stream().collect(Collectors.groupingBy(s -> (s - 1) / 3));
+        List<List<Integer>> subSets = new ArrayList<>(groups.values());
+        List<Integer> lastPartition = subSets.get(2);
+
+        // 通过partition by
+        // partitioningBy 其实是一种特殊的 groupingBy，
+        // 它依照条件测试的是否两种结果来构造返回的数据结构，get(true) 和 get(false) 能即为全部的元素对象
+        Map<Boolean, List<Integer>> groups1 = intList.stream().collect(Collectors.partitioningBy(s -> s > 3));
+        List<List<Integer>> subSets1 = new ArrayList<>(groups.values());
+        List<Integer> lastPartition1 = subSets.get(1);
+
     }
 }
