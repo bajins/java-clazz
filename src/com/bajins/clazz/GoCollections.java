14999aca59bdc90fb4fedbfaeb2acaa5cc8a82eb
woetu <admin@woetu.com>
2020-11-10 14:48:19 +0800

    add Java8 stream将一个list按三个一组分成N个小的list

--- src/com/bajins/clazz/GoCollections.java
+++ src/com/bajins/clazz/GoCollections.java
@@ -1,22 +1,25 @@
 package com.bajins.clazz;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import com.sun.istack.internal.Nullable;
+
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GoCollections {
 
     /**
      * 将一个list按三个一组分成N个小的list
      *
-     * @param list
-     * @param groupSize
+     * @param list      集合数据
+     * @param groupSize 几个分割一组
      * @param <T>
      * @return
      */
     public static <T> List<List<T>> splitList(List<T> list, int groupSize) {
+        if (isEmpty(list)) {
+            return Collections.emptyList();
+        }
         int length = list.size();
         // 计算可以分成多少组
         int num = (length + groupSize - 1) / groupSize;
@@ -29,6 +32,146 @@ public class GoCollections {
             newList.add(list.subList(fromIndex, toIndex));
         }
         return newList;
+    }
+
+    /**
+     * Java8 Stream分割list集合
+     *
+     * @param list      集合数据
+     * @param splitSize 几个分割一组
+     * @return 集合分割后的集合
+     */
+    public static <T> List<List<T>> splitListStream(List<T> list, int splitSize) {
+        //判断集合是否为空
+        if (isEmpty(list)) {
+            return Collections.emptyList();
+        }
+        //计算分割后的大小
+        int maxSize = (list.size() + splitSize - 1) / splitSize;
+        //开始分割
+        return Stream.iterate(0, n -> n + 1)
+                .limit(maxSize)
+                .parallel()
+                .map(a -> list.parallelStream().skip(a * splitSize).limit(splitSize).collect(Collectors.toList()))
+                .filter(b -> !b.isEmpty())
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 分割list集合
+     *
+     * @param list      集合数据
+     * @param splitSize 几个分割一组
+     * @return 集合分割后的集合
+     */
+    public static <T> List<List<T>> splitListOriginal(List<T> list, int splitSize) {
+        if (isEmpty(list)) {
+            return Collections.emptyList();
+        }
+        Map<Integer, List<T>> map = new HashMap<Integer, List<T>>();
+        if (splitSize <= 0) {
+            splitSize = 1;
+        }
+        int k = 0;
+        int size;
+        for (int i = 0; i < (size = list.size()); i += splitSize) {
+            List<T> newList;
+            try {
+                newList = list.subList(i, i + splitSize);
+            } catch (Exception e) {
+                newList = list.subList(i, size);
+            }
+            map.put(k, newList);
+            k++;
+        }
+        List<List<T>> listAll = new ArrayList<List<T>>();
+        for (List<T> value : map.values()) {
+            listAll.add(value);
+        }
+        return listAll;
+    }
+
+    /**
+     * 分割list集合
+     *
+     * @param source 集合数据
+     * @param n      分成n个list
+     * @return 集合分割后的集合
+     */
+    public static <T> List<List<T>> averageAssign(List<T> source, int n) {
+        if (isEmpty(source)) {
+            return Collections.emptyList();
+        }
+        List<List<T>> result = new ArrayList<>();
+        int size = source.size();
+        if (n <= 0) {
+            n = 1;
+        }
+        //先计算出余数
+        int remainder = size % n;
+        //然后是商
+        int number = size / n;
+        //偏移量
+        int offset = 0;
+        for (int i = 0; i < n; i++) {
+            List<T> value;
+            if (remainder > 0) {
+                value = source.subList(i * number + offset, (i + 1) * number + offset + 1);
+                remainder--;
+                offset++;
+            } else {
+                value = source.subList(i * number + offset, (i + 1) * number + offset);
+            }
+            result.add(value);
+        }
+        return result;
+    }
+
+    /**
+     * 分割list集合
+     *
+     * @param source 集合数据
+     * @param n      几个分割一组
+     * @return 集合分割后的集合
+     */
+    public static <T> List<List<T>> averageAssign1(List<T> source, int n) {
+        if (isEmpty(source)) {
+            return Collections.emptyList();
+        }
+        List<List<T>> result = new ArrayList<>();
+        int size = source.size();
+        if (n <= 0) {
+            n = 1;
+        }
+        int remainder = size % n;
+        int number = size / n;
+        for (int i = 0; i < number; i++) {
+            result.add(source.subList(i * n, (i + 1) * n));
+        }
+        if (remainder > 0) {
+            result.add(source.subList(size - remainder, size));
+        }
+        return result;
+    }
+
+    /**
+     * 判断集合是否为空
+     *
+     * @param collection
+     * @return
+     */
+    public static boolean isEmpty(@Nullable Collection<?> collection) {
+        return collection == null || collection.isEmpty();
+    }
+
+    /**
+     * 判断map是否为空
+     *
+     * @param map
+     * @return
+     */
+    public static boolean isEmpty(@Nullable Map<?, ?> map) {
+        return map == null || map.isEmpty();
     }
 
 
