92633dc737865341e0061f04418498c7ba75481f
woetu <admin@woetu.com>
2020-12-12 09:57:57 +0800

    add

---
+++ src/com/bajins/clazz/BeanUtil.java
@@ -0,0 +1,55 @@
+package com.bajins.clazz;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/**
+ * Java语言欠缺属性、事件、多重继承功能。
+ * 如果要在Java程序中实现一些面向对象编程的常见需求，只能手写大量胶水代码/粘合代码（glue code），用途是粘合那些可能不兼容的代码。
+ * <p>
+ * Bean的含义是可重复使用的Java组件，所谓组件就是一个由可以自行进行内部管理的一个或几个类所组成、外界不了解其内部信息和运行方式的群体
+ * <p>
+ * Java Bean正是编写这套胶水代码的惯用模式或约定，必须达到如下条件：
+ * <p>
+ * 1、所有属性为private
+ * 2、提供默认无参构造函数
+ * 3、提供getter和setter
+ * 4、可序列化：实现serializable接口
+ * <p>
+ * https://www.zhihu.com/question/19773379/answers/updated
+ */
+public class BeanUtil {
+    /**
+     * 合并对象
+     *
+     * @param origin      源数据
+     * @param destination 目标对象，合并后数据
+     * @param cover       true为覆盖，false为不覆盖
+     * @param <T>
+     */
+    public <T> void mergeObject(T origin, T destination, boolean cover) throws IllegalAccessException {
+        if (origin == null || destination == null) {
+            return;
+        }
+        // 如果不是同一个类
+        if (!origin.getClass().equals(destination.getClass())) {
+            return;
+        }
+        Field[] fields = destination.getClass().getDeclaredFields();
+        for (int i = 0; i < fields.length; i++) {
+            Field targetField = fields[i];
+            // 如果属性为静态的
+            if (Modifier.isStatic(targetField.getModifiers())) {
+                continue;
+            }
+            targetField.setAccessible(true); // 设置属性可访问
+            Object valueD = targetField.get(origin); // 源数据
+            Object valueO = targetField.get(destination); // 目标数据
+            // 如果源数据不为空，且该属性不为序列化，可覆盖或目标数据为空
+            if (null != valueD && !"serialVersionUID".equals(targetField.getName()) && (cover || null == valueO)) {
+                targetField.set(destination, valueD); // 设值到目标对象属性
+            }
+            targetField.setAccessible(false);
+        }
+    }
+}
