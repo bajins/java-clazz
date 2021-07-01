ddfd8d22426293dbed45bdc14cc521d0f62b3b38
woetu <admin@woetu.com>
2021-07-01 15:16:05 +0800

    update

---
+++ src/com/bajins/clazz/BinOctDecHexTest.java
@@ -0,0 +1,105 @@
+package com.bajins.clazz;
+
+/**
+ * 自定义进制
+ */
+public class BinOctDecHexTest {
+    //设置字符数组
+    //可以添加任意不重复字符，提高能转换的进制的上限
+    static char chs[] = new char[36];
+
+    static {
+        for (int i = 0; i < 10; i++) {
+            chs[i] = (char) ('0' + i);
+        }
+        for (int i = 10; i < chs.length; i++) {
+            chs[i] = (char) ('A' + (i - 10));
+        }
+    }
+
+    /**
+     * 转换方法
+     *
+     * @param num       元数据字符串
+     * @param fromRadix 元数据的进制类型
+     * @param toRadix   目标进制类型
+     * @return
+     */
+    static String transRadix(String num, int fromRadix, int toRadix) {
+        int number = Integer.valueOf(num, fromRadix);
+        StringBuilder sb = new StringBuilder();
+        while (number != 0) {
+            sb.append(chs[number % toRadix]);
+            number = number / toRadix;
+        }
+        return sb.reverse().toString();
+    }
+
+
+    /**
+     * 在进制表示中的字符集合。
+     */
+    final static char[] digits = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
+
+    /**
+     * 将十进制的数字转换为指定进制的字符串。
+     *
+     * @param i      十进制的数字。
+     * @param system 指定的进制，常见的2/8/16。
+     * @return 转换后的字符串。
+     */
+    public static String toCustomNumericString(int i, int system) {
+        long num = 0;
+        if (i < 0) {
+            num = ((long) 2 * 0x7fffffff) + i + 2;
+        } else {
+            num = i;
+        }
+        char[] buf = new char[32];
+        int charPos = 32;
+        while ((num / system) > 0) {
+            buf[--charPos] = digits[(int) (num % system)];
+            num /= system;
+        }
+        buf[--charPos] = digits[(int) (num % system)];
+        return new String(buf, charPos, (32 - charPos));
+    }
+
+    /**
+     * 由于用到了位移操作，所以它的进制表示只是局限于2/4/8/16/32，其它的就不能实现了。
+     *
+     * @param i 十进制的数字
+     * @param shift 当shift为1时，表示二进制；当shift为2时，表示四进制；依次类推
+     * @return
+     */
+    private static String toUnsignedString(int i, int shift) {
+        char[] buf = new char[32];
+        int charPos = 32;
+        int radix = 1 << shift;
+        int mask = radix - 1;
+        do {
+            buf[--charPos] = digits[i & mask];
+            i >>>= shift;
+        } while (i != 0);
+        return new String(buf, charPos, (32 - charPos));
+    }
+
+    //测试
+    public static void main(String[] args) {
+        System.out.println(transRadix("YGL", 36, 26));
+
+        System.out.println(toCustomNumericString(26,26));
+        System.out.println(toUnsignedString(26,32));
+
+        CustomDigit cd = new CustomDigit(750, 16, 1);
+        for (Integer i : cd.getFormattedNumberList()) {
+            // 因为从1开始，所以这边只加上64，即从'A'输出到'Z'
+            System.out.print((char) (i + 64));
+        }
+    }
+
+}
---
+++ src/com/bajins/clazz/CustomDigit.java
@@ -0,0 +1,190 @@
+package com.bajins.clazz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 自定义进制类
+ *
+ * @author 王钟沛
+ * @version 1.0
+ * @category 框架工具实体类
+ */
+public class CustomDigit {
+
+    /**
+     * 十进制数
+     */
+    private Integer decimal;
+
+    /**
+     * 用于储存转换后数据的列表
+     */
+    private List<CustomInteger> numberList = new ArrayList<CustomInteger>();
+
+    /**
+     * 位数，比如2、8、10、16、60等等任意正整数
+     */
+    private Integer digit;
+
+    /**
+     * 最小值，默认为0
+     */
+    private Integer minValue = 0;
+
+    public CustomDigit() {
+        super();
+    }
+
+    public CustomDigit(Integer decimal, Integer digit) {
+        this.decimal = decimal;
+        this.digit = digit;
+        numberList.add(new CustomInteger(0));
+    }
+
+    public CustomDigit(Integer decimal, Integer digit, Integer minValue) {
+        this.decimal = decimal;
+        this.digit = digit;
+        this.minValue = minValue;
+        numberList.add(new CustomInteger(0));
+    }
+
+    /**
+     * @return the decimal
+     */
+    public Integer getDecimal() {
+        return decimal;
+    }
+
+    /**
+     * @param decimal the decimal to set
+     */
+    public void setDecimal(Integer decimal) {
+        this.decimal = decimal;
+    }
+
+    /**
+     * @return the numberList
+     */
+    public List<CustomInteger> getNumberList() {
+        return numberList;
+    }
+
+    /**
+     * @param numberList the numberList to set
+     */
+    public void setNumberList(List<CustomInteger> numberList) {
+        this.numberList = numberList;
+    }
+
+    /**
+     * @return the digit
+     */
+    public Integer getDigit() {
+        return digit;
+    }
+
+    /**
+     * @param digit the digit to set
+     */
+    public void setDigit(Integer digit) {
+        this.digit = digit;
+    }
+
+    /**
+     * @return the minValue
+     */
+    public Integer getMinValue() {
+        return minValue;
+    }
+
+    /**
+     * @param minValue the minValue to set
+     */
+    public void setMinValue(Integer minValue) {
+        this.minValue = minValue;
+    }
+
+    public void clear() {
+        this.decimal = 0;
+        this.digit = 0;
+        this.minValue = 0;
+        this.numberList.clear();
+    }
+
+    public Integer getLength() {
+        return this.numberList.size();
+    }
+
+    private List<CustomInteger> formattedNumberList() {
+        int initialNumber = decimal;
+        while (initialNumber > 0) {
+            int tempNumber = initialNumber > digit ? digit : initialNumber;
+            CustomInteger lastNumber = this.numberList.get(getLength() - 1);
+            lastNumber.setValue(lastNumber.getValue() + tempNumber);
+            initialNumber = initialNumber - tempNumber;
+            arrangementList(getLength() - 1);
+        }
+
+        return this.numberList;
+    }
+
+    private void arrangementList(Integer index) {
+        CustomInteger lastNumber = this.numberList.get(index);
+        if (lastNumber.getValue() >= digit) {
+            boolean flag = false;
+            int value = lastNumber.getValue() - digit + minValue;
+            if (value >= digit) {
+                flag = true;
+                // 防止+minValue之后又再次大于进制的阀值
+                value = value - digit + minValue;
+            }
+            // 如果再次大于阀值，则向上一位进2，否则进1
+            int count = flag ? 2 : 1;
+            lastNumber.setValue(value);
+            if (index == 0) {
+                this.numberList.add(0, new CustomInteger(count));
+                return;
+            } else {
+                CustomInteger preNumber = this.numberList.get(index - 1);
+                preNumber.setValue(preNumber.getValue() + count);
+            }
+        }
+        if (index == 0) {
+            return;
+        }
+        arrangementList(index - 1);
+    }
+
+    public List<Integer> getFormattedNumberList() {
+        List<Integer> list = new ArrayList<Integer>();
+        this.formattedNumberList();
+        for (CustomInteger ci : this.numberList) {
+            list.add(ci.getValue());
+        }
+        return list;
+    }
+}
+
+class CustomInteger {
+    private Integer value;
+
+    public CustomInteger() {
+    }
+
+    public CustomInteger(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
