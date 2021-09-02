1ad06ad851949bddc786b106bef01c7797caa45d
woetu <admin@woetu.com>
2021-09-02 19:37:39 +0800

    update

--- src/com/bajins/clazz/BeanUtil.java
+++ src/com/bajins/clazz/BeanUtil.java
@@ -30,12 +30,14 @@ import java.util.stream.Collectors;
  * 3、提供getter和setter
  * 4、可序列化：实现serializable接口
  * <p>
+ * https://blog.csdn.net/goodbye_youth/category_9124345.html
  *
  * @see java.beans JavaBean是一种特殊的Java类,主要用于传递数据信息,这种Java类中的方法主要用于访问私有字段,且方法名符合某种命名规则，
  * JavaBean的属性是根据其中的setter和getter方法名推断出来的，它根本看不到java类内部的成员变量
  * @see Introspector 内省：对JavaBean类属性、事件的一种缺省处理方法，先得到属性描述器PropertyDecriptor后再进行各种操作
  * @see PropertyDescriptor 属性描述器：通过存储器导出一个JavaBean类的属性
  * https://www.zhihu.com/question/19773379/answers/updated
+ * @see java.lang.reflect.Executable
  */
 public class BeanUtil {
     /**
