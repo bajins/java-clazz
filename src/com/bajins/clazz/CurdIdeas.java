d788476db3d128ee1d251e1f75753fcfa1157806
woetu <admin@woetu.com>
2021-09-26 13:28:51 +0800

    update

---
+++ src/com/bajins/clazz/CurdIdeas.java
@@ -0,0 +1,84 @@
+package com.bajins.clazz;
+
+
+import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * 一些在工作中常用的编程思路（Thinking）
+ */
+public class CurdIdeas {
+
+    /**
+     * 比较一个表单列表在数据库中的数据做增查改删
+     */
+    public void crud() throws IntrospectionException, InvocationTargetException, IllegalAccessException {
+        // 伪代码：表单数据
+        List<DelayedManager.DelayedMessage> formList = new ArrayList<>();
+        formList.add(new DelayedManager.DelayedMessage(1, "1", 21));
+        formList.add(new DelayedManager.DelayedMessage(2, "2", 2)); // 增
+        formList.add(new DelayedManager.DelayedMessage(4, "4", 57)); // 改
+        formList.add(new DelayedManager.DelayedMessage(6, "6", 67));
+        // 伪代码：查询的数据库数据
+        List<DelayedManager.DelayedMessage> dbList = new ArrayList<>();
+        dbList.add(new DelayedManager.DelayedMessage(1, "1", 21));
+        dbList.add(new DelayedManager.DelayedMessage(3, "3", 35)); // 删
+        dbList.add(new DelayedManager.DelayedMessage(4, "4", 53));
+        dbList.add(new DelayedManager.DelayedMessage(6, "6", 67));
+
+        // 把数据库数据list转map，方便接下来的代码获取并比较
+        Map<String, DelayedManager.DelayedMessage> dbMap =
+                dbList.stream().collect(Collectors.toMap(DelayedManager.DelayedMessage::getBody, Function.identity()));
+
+        /*// 可能数据库表需要存每条数据的行号，为避免重复需要过滤出可用数字
+        // 获取数据库中查出的所有已经存在的行号
+        Set<Integer> seqSet = dbList.stream().map(DelayedMessage::getId).collect(Collectors.toSet());
+        // 创建一个指定大小的顺序数字列表
+        List<Integer> numberList =
+                IntStream.range(1, formList.size() + seqSet.size()).boxed().collect(Collectors.toList());
+        // 过滤已有的数字后可用数字
+        List<Integer> availableNumbers =
+                seqSet.stream().filter(item -> !numberList.contains(item)).collect(Collectors.toList());*/
+
+        List<DelayedManager.DelayedMessage> addList = new ArrayList<>(); // 插入的数据
+        List<DelayedManager.DelayedMessage> updateList = new ArrayList<>(); // 更新的数据
+        List<DelayedManager.DelayedMessage> deleteList = null; // 删除的数据
+
+        for (DelayedManager.DelayedMessage form : formList) { // 循环遍历表单数据
+            String name = form.getBody();
+
+            DelayedManager.DelayedMessage dbDto = dbMap.get(name); // 获取数据库数据
+            if (dbDto == null) { // 如果不存在，则需要插入到数据库
+                dbDto = new DelayedManager.DelayedMessage(1, "1", 1);
+                addList.add(dbDto); // 新增的数据
+                continue;
+            }
+            dbMap.remove(name); // 移除数据库中要保留的数据, 最后存在的就是需要删除的数据
+            // 判断数据在数据库中是否相同, 如果相同说明数据没有变化
+            if (BeanUtil.equalsFields(form, dbDto, "id", "body", "executeTime")) {
+                continue;
+            }
+            updateList.add(form); // 有变化的数据
+        }
+        deleteList = new ArrayList<>(dbMap.values()); // 最后需要删除数据库的数据
+        if (deleteList != null && !deleteList.isEmpty()) {
+            // TODO: 请求数据库删除数据
+        }
+        if (addList != null && !addList.isEmpty()) {
+            // TODO: 请求数据库插入数据
+        }
+        if (updateList != null && !updateList.isEmpty()) {
+            // TODO: 请求数据库更新数据
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/bajins/clazz/DelayedManager.java
+++ src/com/bajins/clazz/DelayedManager.java
@@ -1,10 +1,7 @@
-package com.bajins.clazz.delayqueue;
+package com.bajins.clazz;
 
 import java.time.Duration;
-import java.util.concurrent.DelayQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * 延时队列的使用示例
@@ -115,5 +112,122 @@ public class DelayedManager {
             }
         }
 
+    }
+
+    /**
+     * 延迟队列消息
+     *
+     * @author https://www.bajins.com
+     * @program com.bajins.common.utils.delayqueue
+     * @description Message
+     * @create 2019-03-16 21:16
+     */
+    public static class DelayedMessage implements Delayed {
+
+        private int id;
+        // 消息内容
+        private String body;
+        // 延迟时长（单位为纳秒），到期时间点的时间戳，判断延时时长。
+        private long executeTime;
+
+        /**
+         * 传入指定的毫秒进行初始化
+         *
+         * @param id
+         * @param body        内容
+         * @param millisecond 时间量：毫秒
+         */
+        public DelayedMessage(int id, String body, long millisecond) {
+            this.id = id;
+            this.body = body;
+            // 前面是新值单位，后面是传入值的单位：毫秒转纳秒
+            long convert = TimeUnit.NANOSECONDS.convert(millisecond, TimeUnit.MILLISECONDS);
+            this.executeTime = convert + System.nanoTime();
+        }
+
+        /**
+         * 传入指定基于时间的时间量和单位进行初始化
+         *
+         * @param id
+         * @param body         内容
+         * @param amountOfTime 时间量
+         * @param timeUnit     时间单位
+         */
+        public DelayedMessage(int id, String body, long amountOfTime, TimeUnit timeUnit) {
+            this.id = id;
+            this.body = body;
+            // 前面是新值单位，后面是传入值的单位
+            long convert = TimeUnit.NANOSECONDS.convert(amountOfTime, timeUnit);
+            this.executeTime = convert + System.nanoTime();
+        }
+
+        /**
+         * 传入基于时间的时间量进行初始化
+         *
+         * @param id
+         * @param body     内容
+         * @param duration 时间量
+         */
+        public DelayedMessage(int id, String body, Duration duration) {
+            this.id = id;
+            this.body = body;
+            this.executeTime = duration.toNanos() + System.nanoTime();
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+
+        public long getExecuteTime() {
+            return executeTime;
+        }
+
+        public void setExecuteTime(long executeTime) {
+            this.executeTime = executeTime;
+        }
+
+        /**
+         * 返回剩余指定时间到当前时间的时间戳，如果返回的是负数则说明到期，否则还没到期
+         *
+         * @param unit
+         * @return
+         */
+        @Override
+        public long getDelay(TimeUnit unit) {
+            return unit.convert(this.executeTime - System.nanoTime(), TimeUnit.NANOSECONDS);
+        }
+
+        /**
+         * 时间设定小的 优先被消费
+         *
+         * @param delayed
+         * @return
+         */
+        @Override
+        public int compareTo(Delayed delayed) {
+            DelayedMessage o = (DelayedMessage) delayed;
+            return this.executeTime - o.getExecuteTime() > 0 ? 1 : -1;
+        }
+
+        @Override
+        public String toString() {
+            return "DelayedMessage{" +
+                    "id=" + id +
+                    ", body='" + body + '\'' +
+                    ", excuteTime=" + executeTime +
+                    '}';
+        }
     }
 }
